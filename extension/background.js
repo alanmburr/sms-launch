263cc446de79590426f71b7d24488f1543c5e2a8
kitty-kat <54408289+wackyblackie@users.noreply.github.com>
2021-01-30 16:05:03 -0500

    Delete background.js

--- extension/background.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab)
-{	
-	var RedirectURL = "https://wackyblackie.github.io/sms-launch/index.html";
-	chrome.tabs.create({url: RedirectURL});	
-});
